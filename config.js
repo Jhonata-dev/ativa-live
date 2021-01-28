be3654d66cd9e632910bd299ae6c12ae47640405
Jhonaa Gomes <jhonata.fgomes@hotmail.com>
2021-01-28 13:25:12 -0300

    Correção de layout e add event keypress

--- config.js
+++ config.js
@@ -1,12 +1,26 @@
 
-const _nameCond = "EMPRESÁRIOS DO BRASIL";
+const _nameCond = "Empresários do Brasil";
 const _urlCam = "http://betroniccorporation.com:4321/cam_2.cgi";
 
+const exampleKey = 13;
 
 const title = document.getElementById('title');
 const cam = document.getElementById('cam');
+const _container = document.getElementById('container');
 
-title.innerHTML = _nameCond;
+// document.addEventListener('keydown', keyEvent);
+
+console.log(_nameCond);
+title.innerText = _nameCond;
 if (cam)
     cam.src = _urlCam;
 
+
+window.addEventListener('keydown', keyEvent);
+
+function keyEvent(event) {
+    var key = event.keyCode || event.which;
+    if (key == exampleKey) {
+        window.location.assign("index_Firefox.html")
+    }
+}
