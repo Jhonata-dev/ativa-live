75366f11ae7f3e085fc92c7aedd3c63657e900fc
Jhonaa Gomes <jhonata.fgomes@hotmail.com>
2021-10-05 09:23:30 -0300

    Ativa lauyot

--- config.js
+++ config.js
@@ -1,26 +1,23 @@
-
-const _nameCond = "Portaria Betronic";
+const _nameCond = "Portaria ATIVA";
 const _urlCam = "";
 
 const exampleKey = 13;
 
-const title = document.getElementById('title');
-const cam = document.getElementById('cam');
-const _container = document.getElementById('container');
+const title = document.getElementById("title");
+const cam = document.getElementById("cam");
+const _container = document.getElementById("container");
 
 // document.addEventListener('keydown', keyEvent);
 
 console.log(_nameCond);
 title.innerText = _nameCond;
-if (cam)
-    cam.src = _urlCam;
+if (cam) cam.src = _urlCam;
 
-
-window.addEventListener('keydown', keyEvent);
+window.addEventListener("keydown", keyEvent);
 
 function keyEvent(event) {
-    var key = event.keyCode || event.which;
-    if (key == exampleKey) {
-        window.location.assign("index_Firefox.html")
-    }
+  var key = event.keyCode || event.which;
+  if (key == exampleKey) {
+    window.location.assign("index_Firefox.html");
+  }
 }
